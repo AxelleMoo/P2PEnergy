27513ca16a09bd81fc635fbea29e48c3acc9fbf5
Axelle Moortgat <axellemoortgat@Axelles-MacBook-Pro.local>
2023-12-27 14:54:18 +0100

    change port to 8000

--- simulationPage/Dropzone/Dropzone.jsx
+++ simulationPage/Dropzone/Dropzone.jsx
@@ -24,7 +24,7 @@ const Dropzone = ({title, selectedProvider, selectedContract, setTotalSavings}) 
           // console.log(process.env);
 
           const vmIp = "34.79.250.28";
-          const response = await fetch(`http://${vmIp}:3000/upload`, {
+          const response = await fetch(`http://${vmIp}:8000/upload`, {
             method: 'POST',
             body: formData,
           });
